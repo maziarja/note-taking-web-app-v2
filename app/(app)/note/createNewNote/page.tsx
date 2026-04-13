2e3eebd73f6ea62fd0ebfa657bf65c0d2e0533a3
maziarja <maziar.ja@gmail.com>
2026-04-13 12:37:17 -0700

    Wrapping CreateNewNote with Suspense because I got error during deploy

--- app/(app)/note/createNewNote/page.tsx
+++ app/(app)/note/createNewNote/page.tsx
@@ -1,10 +1,13 @@
 import CreateNewNote from "@/components/note/CreateNewNote";
 import Container from "@/components/ui/Container";
+import { Suspense } from "react";
 
 function Page() {
   return (
     <Container className="rounded-t-none">
-      <CreateNewNote />
+      <Suspense fallback={null}>
+        <CreateNewNote />
+      </Suspense>
     </Container>
   );
 }
