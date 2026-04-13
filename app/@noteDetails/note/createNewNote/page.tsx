1aa0ee3645a60d65ee38aea31a725d4ed3feb3e9
maziarja <maziar.ja@gmail.com>
2026-04-13 14:21:51 -0700

    tring to fix

--- app/@noteDetails/note/createNewNote/page.tsx
+++ app/@noteDetails/note/createNewNote/page.tsx
@@ -1,11 +1,11 @@
-import CreateNewNote from "@/components/note/CreateNewNote";
+// import CreateNewNote from "@/components/note/CreateNewNote";
 import { Suspense } from "react";
 
 function Page() {
   return (
     <div className="hidden border-r px-6 py-5 lg:block">
-      <Suspense fallback={null}>
-        <CreateNewNote isDesktop={true} />
+      <Suspense fallback={<p>loading...</p>}>
+        {/* <CreateNewNote isDesktop={true} /> */}
       </Suspense>
     </div>
   );
